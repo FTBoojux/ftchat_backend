8e18d2c9e5b4c9eb2012cf72d7369c52b48bf3e7
Boojux <iamboojux@gmail.com>
2023-06-16 17:02:01 +0800

    now can remove gpt conversation

--- ftchat/service/gpt_conversation.py
+++ ftchat/service/gpt_conversation.py
@@ -12,3 +12,11 @@ def get_conversation_list(user_id):
     conversation_qs = GptConversation.objects.filter(user_id=user_id)
     conversation_list = [model_to_dict(conversation) for conversation in conversation_qs]
     return conversation_list
+
+def delete_conversation(user_id, conversation_id):
+    conversation = GptConversation.objects.get(conversation_id=conversation_id)
+    if conversation.user_id == user_id:
+        conversation.delete()
+        return True
+    else:
+        return False
--- ftchat/views/gpt/gpt_views.py
+++ ftchat/views/gpt/gpt_views.py
@@ -12,9 +12,17 @@ class ConversationView(AuthenticateView):
         uid = jwt_utils.get_uid_from_jwt(jwt_utils.get_token_from_bearer(token))
         res = gpt_conversation_service.create_conversation(uid,title)
         return JsonResponse({'result':'success','message':'','code':200,'data':res})
+    
     def get(self,request,*args,**kwargs):
         # 获取header中的token
         token = request.META.get('HTTP_AUTHORIZATION')
         uid = jwt_utils.get_uid_from_jwt(jwt_utils.get_token_from_bearer(token))
         res = gpt_conversation_service.get_conversation_list(uid)
+        return JsonResponse({'result':'success','message':'','code':200,'data':res})
+    
+    def delete(self,request,*args,**kwargs):
+        token = request.META.get('HTTP_AUTHORIZATION')
+        uid = jwt_utils.get_uid_from_jwt(jwt_utils.get_token_from_bearer(token))
+        conversation_id = request.data.get('conversation_id')
+        res = gpt_conversation_service.delete_conversation(uid,conversation_id)
         return JsonResponse({'result':'success','message':'','code':200,'data':res})
