8826a21c937ac91e4d142bee5c47b71748d2f98d
Boojux <iamboojux@gmail.com>
2023-06-29 17:59:50 +0800

    optimized query of contact and friend

--- ftchat/service/account.py
+++ ftchat/service/account.py
@@ -7,44 +7,40 @@ from django.db.models import Q
 from django.db import connection
 
 def search_contact(user_id,keyword):
-    # friend_ids = Contact.objects.filter(user=user_id).values_list('friend', flat=True)
-    # users = User.objects.filter(
-    #     Q(user_id__in=friend_ids) &
-    #     Q(username__icontains=keyword)
-    # ).values('user_id', 'username', 'avatar', 'bio')
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT ftchat_user.username AS username, ftchat_user.user_id AS user_id, ftchat_user.avatar AS avatar, ftchat_user.bio AS bio 
-            FROM ftchat_user 
-            INNER JOIN ftchat_contact ON ftchat_contact.friend = ftchat_user.user_id
-            AND ftchat_contact.user = %s 
-            WHERE user_id != %s 
-            AND user_id != 1
-            AND username LIKE %s
-        """, [user_id, user_id, '%' + keyword + '%'])
-        rows = cursor.fetchall()
-    # 获取查询结果的列名
-    column_names = [col[0] for col in cursor.description]
-    # 把查询结果转换成字典形式
-    results = [dict(zip(column_names, row)) for row in rows]
-    return results
+    # with connection.cursor() as cursor:
+    #     cursor.execute("""
+    #         SELECT username, user_id, avatar, bio
+    #         FROM ftchat_user 
+    #         where user_id in(
+	# 			select friend from ftchat_contact where user = %s
+    #         ) and user_id != '1' and user_id != %s and username like %s
+    #     """, [user_id, user_id, '%' + keyword + '%'])
+    #     rows = cursor.fetchall()
+    # # 获取查询结果的列名
+    # column_names = [col[0] for col in cursor.description]
+    # # 把查询结果转换成字典形式
+    # results = [dict(zip(column_names, row)) for row in rows]
+    # return results
+    contact_ids = Contact.objects.filter(user=user_id).values_list('friend', flat=True)
+    results = User.objects.filter(Q(user_id__in=contact_ids), ~Q(user_id=user_id), ~Q(user_id=1), username__icontains=keyword).values('username', 'user_id', 'avatar', 'bio')
+    return list(results)
 
 def search_stranger(user_id,keyword):
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT ftchat_user.username AS username, ftchat_user.user_id AS user_id, ftchat_user.avatar AS avatar, ftchat_user.bio AS bio 
-            FROM ftchat_user 
-            LEFT JOIN ftchat_contact ON ftchat_contact.friend = ftchat_user.user_id
-            AND ftchat_contact.user != %s
-            WHERE 
-            user_id != %s
-            AND user_id != 1
-            AND username LIKE %s
-        """, [user_id, user_id, '%' + keyword + '%'])
-        rows = cursor.fetchall()
-    column_names = [col[0] for col in cursor.description]
-    results = [dict(zip(column_names, row)) for row in rows]
-    return results
+    # with connection.cursor() as cursor:
+    #     cursor.execute("""
+    #         SELECT username, user_id, avatar, bio
+    #         FROM ftchat_user 
+    #         where user_id not in(
+	# 			select friend from ftchat_contact where user = %s
+    #         ) and user_id != '1' and user_id != %s and username like %s
+    #     """, [user_id, user_id, '%' + keyword + '%'])
+    #     rows = cursor.fetchall()
+    # column_names = [col[0] for col in cursor.description]
+    # results = [dict(zip(column_names, row)) for row in rows]
+    # return results
+    contact_ids = Contact.objects.filter(user=user_id).values_list('friend', flat=True)
+    results = User.objects.exclude(Q(user_id__in=contact_ids) | Q(user_id=user_id) | Q(user_id=1)).filter(username__icontains=keyword).values('username', 'user_id', 'avatar', 'bio')
+    return list(results)
 
 def save_contact_request(uid, target, message):
     if Contact.objects.filter(user=uid, friend=target).exists():
