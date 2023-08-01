979ab5e8256c8f12cbe4d55282dec8b11b836c8a
Boojux <iamboojux@gmail.com>
2023-08-01 10:07:51 +0800

    add grp join requests num

--- ftchat/service/message.py
+++ ftchat/service/message.py
@@ -1,5 +1,9 @@
-from ftchat.models import ContactRequest
+from ftchat.models import ContactRequest,GroupJoinRequest,Group
 
 def get_message_nums(uid):
     contacts = ContactRequest.objects.filter(receiver=uid,status="pending").count()
-    return {"contacts":contacts,"messages":0}
+    # 查找所有由该用户创建的群聊
+    groups = Group.objects.filter(owner=uid)
+    # 查找所有由该用户创建的群聊的加群请求
+    group_join_requests = GroupJoinRequest.objects.filter(group__in=groups,status="pending")
+    return {"contacts":contacts+groups,"messages":0}
