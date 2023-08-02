d625cdb31f54ae8104799ea212592d91983515dc
Boojux <iamboojux@gmail.com>
2023-08-02 09:25:54 +0800

    bug fix

--- ftchat/service/message.py
+++ ftchat/service/message.py
@@ -5,5 +5,5 @@ def get_message_nums(uid):
     # 查找所有由该用户创建的群聊
     groups = Group.objects.filter(owner=uid)
     # 查找所有由该用户创建的群聊的加群请求
-    group_join_requests = GroupJoinRequest.objects.filter(group__in=groups,status="pending")
-    return {"contacts":contacts+groups,"messages":0}
+    group_join_requests = GroupJoinRequest.objects.filter(group__in=groups,status="pending").count()    
+    return {"contacts":contacts+group_join_requests,"messages":0}
